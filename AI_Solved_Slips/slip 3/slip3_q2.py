62f79adbd09aa385f440ab31ebede4499ad41701
Sumit Pagare <150526313+SumitPagare@users.noreply.github.com>
2023-12-14 00:32:17 +0530

    slip3_q2.py

--- AI_Solved_Slips/slip 3/slip3_q2.py
+++ AI_Solved_Slips/slip 3/slip3_q2.py
@@ -1,57 +1,32 @@
-class Graph:
-    def __init__(self):
-        self.graph = {}
+graph = {
+  '1' : ['2','3'],
+  '2' : ['4'],
+  '3' : ['2'],
+  '4' : ['5', '6'],
+  '5' : ['3', '7'],
+  '6' : [],
+  '7' : ['6']
+}
 
-    def add_edge(self, node, neighbor):
-        if node not in self.graph:
-            self.graph[node] = []
-        self.graph[node].append(neighbor)
+def dfs_path(graph, start, goal, path=[]):
+    path = path + [start]
+    if start == goal:
+        return path
+    if start not in graph:
+        return None
+    for neighbor in graph[start]:
+        if neighbor not in path:
+            new_path = dfs_path(graph, neighbor, goal, path)
+            if new_path:
+                return new_path
+    return None
 
-    def dfs(self, start, goal):
-        visited = set()
+# Driver Code
+start_node = '1'
+goal_node = '7'
+result_path = dfs_path(graph, start_node, goal_node)
 
-        def dfs_recursive(node):
-            visited.add(node)
-            print(node, end=" ")
-
-            if node == goal:
-                print("\nGoal node reached!")
-                return True
-
-            for neighbor in self.graph.get(node, []):
-                if neighbor not in visited:
-                    if dfs_recursive(neighbor):
-                        return True
-
-            return False
-
-        if start not in self.graph:
-            print("Start node not found in the graph.")
-            return
-
-        print("Depth First Search:")
-        dfs_recursive(start)
-
-# Example graph
-graph = Graph()
-graph.add_edge(2, 1)
-graph.add_edge(2, 4)
-graph.add_edge(2, 5)
-graph.add_edge(1, 3)
-graph.add_edge(1, 4)
-graph.add_edge(3, 4)
-graph.add_edge(4, 7)
-graph.add_edge(5, 6)
-graph.add_edge(5, 7)
-graph.add_edge(6, 5)
-graph.add_edge(6, 7)
-#graph.add_edge(7, 7)
-#graph.add_edge(6, 8)
-#graph.add_edge(7, 6)
-
-# Specify the initial and goal nodes
-initial_node = 2
-goal_node = 7
-
-# Perform DFS
-graph.dfs(initial_node, goal_node)
+if result_path:
+    print(f"Path from {start_node} to {goal_node}: {result_path}")
+else:
+    print(f"No path found from {start_node} to {goal_node}")
