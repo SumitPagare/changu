b24caa90026f0279401c9f18a062cf2bf968ce0b
Sumit Pagare <150526313+SumitPagare@users.noreply.github.com>
2023-12-14 00:19:24 +0530

    slip5_q2.py

--- AI_Solved_Slips/slip 5/slip5_q2.py
+++ AI_Solved_Slips/slip 5/slip5_q2.py
@@ -1,53 +1,30 @@
-from collections import deque
+graph = {
+  '1' : ['2','4'],
+  '2' : ['3'],
+  '3' : ['4', '6', '5'],
+  '4' : ['2'],
+  '5' : ['7', '8'],
+  '6' : ['8'],
+  '7' : ['8'],
+  '8' : []
+}
 
-class Graph:
-    def __init__(self):
-        self.graph = {}
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
 
-    def add_edge(self, node, neighbor):
-        if node not in self.graph:
-            self.graph[node] = []
-        self.graph[node].append(neighbor)
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
 
-    def bfs(self, start, goal):
-        visited = set()
-        queue = deque([start])
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
 
-        while queue:
-            current_node = queue.popleft()
-            if current_node in visited:
-                continue
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
 
-            visited.add(current_node)
-            print(current_node, end=" ")
-
-            if current_node == goal:
-                print("\nGoal node reached!")
-                return True
-
-            neighbors = self.graph.get(current_node, [])
-            queue.extend(neighbor for neighbor in neighbors if neighbor not in visited)
-
-        print("\nGoal node not reached.")
-        return False
-
-# Example graph
-graph = Graph()
-graph.add_edge(1, 2)
-graph.add_edge(1, 4)
-graph.add_edge(2, 3)
-graph.add_edge(3, 4)
-graph.add_edge(3, 5)
-graph.add_edge(3, 6)
-graph.add_edge(4, 2)
-graph.add_edge(5, 7)
-graph.add_edge(5, 8)
-graph.add_edge(6, 8)
-graph.add_edge(7, 8)
-
-# Specify the initial and goal nodes
-initial_node = 1
-goal_node = 8
-
-# Perform BFS
-graph.bfs(initial_node, goal_node)
+# Driver Code
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '1')    # function calling
